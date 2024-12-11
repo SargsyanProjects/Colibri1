f393b7ab93e1ccf5d344e11476a41222174537c2
Gor Sargsyan <gorrssargsyan@gmail.com>
2024-12-11 12:28:22 +0400

    Connected To Database

---
+++ EduConnectJSP/src/main/java/ru/educonnect/ConnectDB.java
@@ -0,0 +1,12 @@
+package ru.educonnect;
+import java.sql.*;
+
+public class ConnectDB {
+    public static Connection connect() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn= null;
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/educonnect", "root", "root");
+        System.out.println("ConnectDB");
+        return conn;
+    }
+}
--- EduConnectJSP/src/main/java/ru/educonnect/ModeratorDAO.java
+++ EduConnectJSP/src/main/java/ru/educonnect/ModeratorDAO.java
@@ -1,4 +1,34 @@
 package ru.educonnect;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import jdk.internal.icu.text.NormalizerBase;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModeratorDAO {
+
+    List<Moderator> moderators = new ArrayList<>();
+
+    public List<Moderator> getModerators() {
+        return moderators;
+    }
+
+    public void setModerators(List<Moderator> moderators) {
+        this.moderators = moderators;
+    }
+
+    public void checkModerators(String username, String password) throws SQLException, ClassNotFoundException {
+        Statement statement = ConnectDB.connect().createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from moderators where username= '" + username + "' and password = '" + password + "'");
+        while(resultSet.next()) {
+            moderators.add(new Moderator(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3)));
+        }
+
+
+    }
+
 }
